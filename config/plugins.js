c75fbec289b0b5e5b21ce052cff12dafd7314db9
mninozebra <mni@nozebra.dk>
2022-05-14 17:24:56 +0200

    Removed stuff

--- config/plugins.js
+++ config/plugins.js
@@ -9,8 +9,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  "content-export-import": {
-    enabled: true,
-    resolve: "./src/plugins/content-export-import", // path to plugin folder
-  },
 });
